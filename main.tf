0ac0f0631fc26fa35b1ae560ae02e28ffb411f39
jhussain <jhussain833@gmail.com>
2024-08-27 15:03:24 +0100

    update

--- main.tf
+++ main.tf
@@ -4,3 +4,21 @@ provider "aws" {
 resource "aws_s3_bucket" "terraformjamalbucket" {
   bucket = "terraformjamalbucket"
 }
+terraform{
+  backend "s3" {
+
+    bucket    = "terraformjamalbucket"
+    key       = "global/s3/terraform.tfstate"
+    region    = "eu-west-2"
+  }
+}
+resource "aws_instance" "test6" {
+
+  ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
+
+  instance_type = "t2.micro"  # Change to the desired instance type
+  tags = {
+
+    Name = "project1instance" 
+}
+}
